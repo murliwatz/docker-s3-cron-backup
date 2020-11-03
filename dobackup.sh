6259a5651a7c4133d0d55e7e2f4d2d7d5c92bff2
paul <Paul Prll>
2020-11-03 09:34:34 +0100

    fda

--- dobackup.sh
+++ dobackup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FILE_NAME=/tmp/$BACKUP_NAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz
 echo "creating archive"
-tar -zcvf $FILE_NAME /data
+tar -zcvf $FILE_NAME /data$SUB_PATH
 echo "uploading archive to S3 [$FILE_NAME]"
 echo "aws s3 cp $FILE_NAME $S3_BUCKET_URL --endpoint-url=$S3_ENDPOINT --region $S3_REGION"
 aws s3 cp $FILE_NAME $S3_BUCKET_URL --endpoint-url=$S3_ENDPOINT --region $S3_REGION
